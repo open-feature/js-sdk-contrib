ffaf6cc0eebb11d1d2a203e63c200759fe8f5e54
Michael Beemer <michael.beemer@dynatrace.com>
2022-06-09 13:18:45 -0400

    Added OTel hook

---
+++ libs/hooks/opentelemetry/README.md
@@ -0,0 +1,11 @@
+# hooks-opentelemetry
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build hooks-opentelemetry` to build the library.
+
+## Running unit tests
+
+Run `nx test hooks-opentelemetry` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/hooks/opentelemetry/src/lib/hooks-opentelemetry.ts
@@ -0,0 +1,66 @@
+import {
+  Hook,
+  HookContext,
+  EvaluationDetails,
+  FlagValue,
+} from '@openfeature/nodejs-sdk';
+import { Span, Tracer, trace } from '@opentelemetry/api';
+
+const SpanProperties = Object.freeze({
+  FLAG_KEY: 'feature_flag.flag_key',
+  CLIENT_NAME: 'feature_flag.client.name',
+  CLIENT_VERSION: 'feature_flag.client.version',
+  PROVIDER_NAME: 'feature_flag.provider.name',
+  VARIANT: 'feature_flag.evaluated.variant',
+  VALUE: 'feature_flag.evaluated.value',
+});
+
+export class OpenTelemetryHook implements Hook {
+  private spanMap = new WeakMap<HookContext, Span>();
+  private tracer: Tracer;
+
+  // TODO explain what the OTel hook does and how name/version is used.
+  constructor(name: string, version?: string) {
+    this.tracer = trace.getTracer(name, version);
+  }
+
+  before(hookContext: HookContext) {
+    const span = this.tracer.startSpan(
+      `feature flag - ${hookContext.flagValueType}`
+    );
+    span.setAttributes({
+      [SpanProperties.FLAG_KEY]: hookContext.flagKey,
+      [SpanProperties.CLIENT_NAME]: hookContext.client.name,
+      [SpanProperties.CLIENT_VERSION]: hookContext.client.version,
+      [SpanProperties.PROVIDER_NAME]: hookContext.provider.name,
+    });
+
+    this.spanMap.set(hookContext, span);
+  }
+
+  after(
+    hookContext: HookContext,
+    evaluationDetails: EvaluationDetails<FlagValue>
+  ) {
+    if (evaluationDetails.variant) {
+      this.spanMap
+        .get(hookContext)
+        ?.setAttribute(SpanProperties.VARIANT, evaluationDetails.variant);
+    } else {
+      this.spanMap
+        .get(hookContext)
+        ?.setAttribute(
+          SpanProperties.VALUE,
+          JSON.stringify(evaluationDetails.value)
+        );
+    }
+  }
+
+  error(hookContext: HookContext, err: Error) {
+    this.spanMap.get(hookContext)?.recordException(err);
+  }
+
+  finally(hookContext: HookContext) {
+    this.spanMap.get(hookContext)?.end();
+  }
+}
