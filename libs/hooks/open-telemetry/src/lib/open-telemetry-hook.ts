260740b9094f62534aaa8dc6feadf364e5a9d02a
Todd Baert <toddbaert@gmail.com>
2022-07-14 15:19:45 -0400

    feat: i did a thing (#30)

--- libs/hooks/open-telemetry/src/lib/open-telemetry-hook.ts
+++ libs/hooks/open-telemetry/src/lib/open-telemetry-hook.ts
@@ -20,6 +20,7 @@ export class OpenTelemetryHook implements Hook {
   private tracer: Tracer;
 
   constructor(name: string, version?: string) {
+    console.log('test');
     this.tracer = trace.getTracer(name, version);
   }
 
