47c59a9cf82f14a384a83ac4426737d7d26a422d
Todd Baert <toddbaert@gmail.com>
2022-07-15 15:18:08 -0400

    Release plz (#59)
    * Add release-plz
    
    * Add stub provider

---
+++ libs/providers/stub/src/lib/stub-provider.ts
@@ -0,0 +1,50 @@
+import {
+  EvaluationContext,
+  FlagEvaluationOptions,
+  Provider,
+  ResolutionDetails,
+} from '@openfeature/nodejs-sdk';
+
+export class StubProvider implements Provider {
+  metadata = {
+    name: StubProvider.name,
+  };
+
+  contextTransformer = () => ({});
+
+  resolveBooleanEvaluation(
+    flagKey: string,
+    defaultValue: boolean,
+    transformedContext: EvaluationContext,
+    options: FlagEvaluationOptions
+  ): Promise<ResolutionDetails<boolean>> {
+    throw new Error('Method not implemented.');
+  }
+
+  resolveStringEvaluation(
+    flagKey: string,
+    defaultValue: string,
+    transformedContext: EvaluationContext,
+    options: FlagEvaluationOptions
+  ): Promise<ResolutionDetails<string>> {
+    throw new Error('Method not implemented.');
+  }
+
+  resolveNumberEvaluation(
+    flagKey: string,
+    defaultValue: number,
+    transformedContext: EvaluationContext,
+    options: FlagEvaluationOptions
+  ): Promise<ResolutionDetails<number>> {
+    throw new Error('Method not implemented.');
+  }
+
+  resolveObjectEvaluation<U extends object>(
+    flagKey: string,
+    defaultValue: U,
+    transformedContext: EvaluationContext,
+    options: FlagEvaluationOptions
+  ): Promise<ResolutionDetails<U>> {
+    throw new Error('Method not implemented.');
+  }
+}
