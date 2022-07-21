cba6577220d88c290c3745d0a7c80f1ac6d6d063
James-Milligan <james@omnant.co.uk>
2022-07-21 10:43:59 +0100

    nx format:write
    Signed-off-by: James-Milligan <james@omnant.co.uk>

--- libs/providers/flagd/src/index.ts
+++ libs/providers/flagd/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/flagd-provider';
+export * from './lib/service/grpc/service';
+export * from './lib/service/http/service';
--- libs/providers/flagd/src/lib/flagd-provider.spec.ts
+++ libs/providers/flagd/src/lib/flagd-provider.spec.ts
@@ -2,6 +2,6 @@ import { FlagdProvider } from './flagd-provider';
 
 describe('FlagdProvider', () => {
   it('should be and instance of FlagdProvider', () => {
-    expect(new FlagdProvider({})).toBeInstanceOf(FlagdProvider);
+    expect(new FlagdProvider()).toBeInstanceOf(FlagdProvider);
   });
 });
