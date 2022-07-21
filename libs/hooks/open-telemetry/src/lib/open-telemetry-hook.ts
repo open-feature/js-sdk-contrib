0dd780271fabd7aa7c503a48bff75bebb63b46b9
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-07-21 11:55:21 -0400

    feat: Update OTel hook to latest semantic convention (#65)
    Signed-off-by: Michael Beemer <michael.beemer@dynatrace.com>

--- libs/hooks/open-telemetry/src/lib/open-telemetry-hook.ts
+++ libs/hooks/open-telemetry/src/lib/open-telemetry-hook.ts
@@ -8,32 +8,32 @@ import { Span, Tracer, trace } from '@opentelemetry/api';
 
 const SpanProperties = Object.freeze({
   FLAG_KEY: 'feature_flag.flag_key',
-  CLIENT_NAME: 'feature_flag.client.name',
-  CLIENT_VERSION: 'feature_flag.client.version',
-  PROVIDER_NAME: 'feature_flag.provider.name',
-  VARIANT: 'feature_flag.evaluated.variant',
-  VALUE: 'feature_flag.evaluated.value',
+  PROVIDER_NAME: 'feature_flag.provider_name',
+  VARIANT: 'feature_flag.evaluated_variant',
+  VALUE: 'feature_flag.evaluated_value',
 });
 
 export class OpenTelemetryHook implements Hook {
   private spanMap = new WeakMap<HookContext, Span>();
   private tracer: Tracer;
 
-  constructor(name: string, version?: string) {
-    this.tracer = trace.getTracer(name, version);
+  constructor() {
+    this.tracer = trace.getTracer(
+      '@openfeature/open-telemetry-hook',
+      '1.0.6-alpha' // x-release-please-version
+    );
   }
 
   before(hookContext: HookContext) {
     const span = this.tracer.startSpan(
-      `feature flag - ${hookContext.flagValueType}`
+      `${hookContext.providerMetadata.name} ${hookContext.flagKey}`,
+      {
+        attributes: {
+          [SpanProperties.FLAG_KEY]: hookContext.flagKey,
+          [SpanProperties.PROVIDER_NAME]: hookContext.providerMetadata.name,
+        },
+      }
     );
-
-    span.setAttributes({
-      [SpanProperties.FLAG_KEY]: hookContext.flagKey,
-      [SpanProperties.CLIENT_NAME]: hookContext.clientMetadata.name,
-      [SpanProperties.CLIENT_VERSION]: hookContext.clientMetadata.version,
-      [SpanProperties.PROVIDER_NAME]: hookContext.providerMetadata.name,
-    });
 
     this.spanMap.set(hookContext, span);
   }
@@ -47,12 +47,11 @@ export class OpenTelemetryHook implements Hook {
         .get(hookContext)
         ?.setAttribute(SpanProperties.VARIANT, evaluationDetails.variant);
     } else {
-      this.spanMap
-        .get(hookContext)
-        ?.setAttribute(
-          SpanProperties.VALUE,
-          JSON.stringify(evaluationDetails.value)
-        );
+      const value =
+        typeof evaluationDetails.value === 'string'
+          ? evaluationDetails.value
+          : JSON.stringify(evaluationDetails.value);
+      this.spanMap.get(hookContext)?.setAttribute(SpanProperties.VALUE, value);
     }
   }
 
