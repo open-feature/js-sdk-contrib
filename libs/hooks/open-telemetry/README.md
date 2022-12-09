4845b3a1a83a639ede3b2d79b83877262a91e681
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-12-09 12:27:20 -0500

    update otel hook to be spec complient
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- libs/hooks/open-telemetry/README.md
+++ libs/hooks/open-telemetry/README.md
@@ -1,21 +1,59 @@
 # OpenTelemetry Hook
 
+The OpenTelemetry hook for OpenFeature provides a [spec compliant][otel-spec] way to automatically add a feature flag evaluation to a span as a span event. Since feature flags are dynamic and affect runtime behavior, it’s important to collect relevant feature flag telemetry signals. This can be used to determine the impact a feature has on a request, enabling enhanced observability use cases, such as A/B testing or progressive feature releases.
+
 ## Installation
 
 ```
 $ npm install @openfeature/open-telemetry-hook
 ```
 
-Required peer dependencies
+### Peer dependencies
+
+Confirm that the following peer dependencies are installed.
 
 ```
 $ npm install @openfeature/js-sdk @opentelemetry/api
 ```
 
-## Building
+## Usage
+
+OpenFeature provider various ways to register hooks. The location that a hook is registered affects when the hook is run. It's recommended to register the `OpenTelemetryHook` globally in most situations but it's possible to only enable the hook on specific clients. You should **never** register the `OpenTelemetryHook` globally and on a client.
+
+More information on hooks can be found in the [OpenFeature documentation][hook-concept].
+
+### Register Globally
+
+The `OpenTelemetryHook` can be set on the OpenFeature singleton. This will ensure that every flag evaluation will always create a span event, if am active span is available.
+
+```typescript
+import { OpenFeature } from '@openfeature/js-sdk';
+import { OpenTelemetryHook } from '@openfeature/open-telemetry-hook';
+
+OpenFeature.addHooks(new OpenTelemetryHook());
+```
+
+### Register Per Client
+
+The `OpenTelemetryHook` can be set on an individual client. This should only be done if it wasn't set globally and other clients shouldn't use this hook. Setting the hook on the client will ensure that every flag evaluation performed by this client will always create a span event, if am active span is available.
+
+```typescript
+import { OpenFeature } from '@openfeature/js-sdk';
+import { OpenTelemetryHook } from '@openfeature/open-telemetry-hook';
+
+const client = OpenFeature.getClient('my-app');
+client.addHooks(new OpenTelemetryHook());
+```
+
+## Development
+
+### Building
 
 Run `nx package hooks-open-telemetry` to build the library.
 
-## Running unit tests
+### Running unit tests
 
 Run `nx test hooks-open-telemetry` to execute the unit tests via [Jest](https://jestjs.io).
+
+[otel-spec]: https://opentelemetry.io/docs/reference/specification/trace/semantic_conventions/feature-flags/
+[hook-concept]: https://docs.openfeature.dev/docs/reference/concepts/hooks
--- libs/hooks/open-telemetry/src/lib/open-telemetry-hook.spec.ts
+++ libs/hooks/open-telemetry/src/lib/open-telemetry-hook.spec.ts
@@ -1,20 +1,13 @@
 import { EvaluationDetails, HookContext } from '@openfeature/js-sdk';
 
-const setAttributes = jest.fn();
-const setAttribute = jest.fn();
+const addEvent = jest.fn();
 const recordException = jest.fn();
-const end = jest.fn();
-const startSpan = jest.fn(() => ({
-  setAttributes,
-  setAttribute,
-  recordException,
-  end,
-}));
-const getTracer = jest.fn(() => ({ startSpan }));
+
+const getActiveSpan = jest.fn<any, any>(() => ({ addEvent, recordException }));
 
 jest.mock('@opentelemetry/api', () => ({
   trace: {
-    getTracer,
+    getActiveSpan,
   },
 }));
 
@@ -33,7 +26,7 @@ describe('OpenTelemetry Hooks', () => {
     context: {},
     defaultValue: true,
     flagValueType: 'boolean',
-    logger: console
+    logger: console,
   };
 
   let otelHook: OpenTelemetryHook;
@@ -46,73 +39,36 @@ describe('OpenTelemetry Hooks', () => {
     jest.clearAllMocks();
   });
 
-  it('should use the same span with all the hooks', () => {
-    const evaluationDetails: EvaluationDetails<boolean> = {
-      flagKey: hookContext.flagKey,
-
-      value: true,
-    };
-
-    const setSpanMapSpy = jest.spyOn(otelHook['spanMap'], 'set');
-    const testError = new Error();
-
-    otelHook.before(hookContext);
-    expect(setSpanMapSpy).toBeCalled();
-
-    otelHook.after(hookContext, evaluationDetails);
-    expect(setAttribute).toBeCalledWith('feature_flag.evaluated_value', 'true');
-
-    otelHook.error(hookContext, testError);
-    expect(recordException).toBeCalledWith(testError);
-
-    otelHook.finally(hookContext);
-    expect(end).toBeCalled();
-  });
-
-  describe('before hook', () => {
-    it('should start a new span', () => {
-      expect(otelHook.before(hookContext)).toBeUndefined();
-      expect(getTracer).toBeCalled();
-      expect(startSpan).toBeCalledWith('testProvider testFlagKey', {
-        attributes: {
-          'feature_flag.flag_key': 'testFlagKey',
-          'feature_flag.provider_name': 'testProvider',
-        },
-      });
-      expect(otelHook['spanMap'].has(hookContext)).toBeTruthy;
-    });
-  });
-
   describe('after hook', () => {
-    it('should set the variant as a span attribute', () => {
+    it('should use the variant value on the span event', () => {
       const evaluationDetails: EvaluationDetails<boolean> = {
         flagKey: hookContext.flagKey,
         value: true,
         variant: 'enabled',
-      }; // The before hook should
+      };
 
-      otelHook.before(hookContext);
       otelHook.after(hookContext, evaluationDetails);
 
-      expect(setAttribute).toBeCalledWith(
-        'feature_flag.evaluated_variant',
-        'enabled'
-      );
+      expect(addEvent).toBeCalledWith('feature_flag', {
+        'feature_flag.key': 'testFlagKey',
+        'feature_flag.provider_name': 'testProvider',
+        'feature_flag.variant': 'enabled',
+      });
     });
 
-    it('should set the value as a span attribute', () => {
+    it('should use a stringified value as the variant value on the span event', () => {
       const evaluationDetails: EvaluationDetails<boolean> = {
         flagKey: hookContext.flagKey,
         value: true,
-      }; // The before hook should
+      };
 
-      otelHook.before(hookContext);
       otelHook.after(hookContext, evaluationDetails);
 
-      expect(setAttribute).toBeCalledWith(
-        'feature_flag.evaluated_value',
-        'true'
-      );
+      expect(addEvent).toBeCalledWith('feature_flag', {
+        'feature_flag.key': 'testFlagKey',
+        'feature_flag.provider_name': 'testProvider',
+        'feature_flag.variant': 'true',
+      });
     });
 
     it('should set the value without extra quotes if value is already a string', () => {
@@ -120,46 +76,40 @@ describe('OpenTelemetry Hooks', () => {
         flagKey: hookContext.flagKey,
         value: 'already-string',
       };
-
-      otelHook.before(hookContext);
       otelHook.after(hookContext, evaluationDetails);
 
-      expect(setAttribute).toBeCalledWith(
-        'feature_flag.evaluated_value',
-        'already-string'
-      );
+      expect(addEvent).toBeCalledWith('feature_flag', {
+        'feature_flag.key': 'testFlagKey',
+        'feature_flag.provider_name': 'testProvider',
+        'feature_flag.variant': 'already-string',
+      });
+    });
+
+    it('should not call addEvent because there is no active span', () => {
+      getActiveSpan.mockReturnValueOnce(undefined);
+      const evaluationDetails: EvaluationDetails<boolean> = {
+        flagKey: hookContext.flagKey,
+        value: true,
+        variant: 'enabled',
+      };
+
+      otelHook.after(hookContext, evaluationDetails);
+      expect(addEvent).not.toBeCalled();
     });
   });
 
   describe('error hook', () => {
     const testError = new Error();
 
-    it('should not call recordException because the span is undefined', () => {
-      otelHook.error(hookContext, testError);
-      expect(otelHook['spanMap'].has(hookContext)).toBeFalsy;
-      expect(recordException).not.toBeCalledWith(testError);
-    });
-
     it('should call recordException with a test error', () => {
-      otelHook.before(hookContext);
       otelHook.error(hookContext, testError);
-      expect(otelHook['spanMap'].has(hookContext)).toBeTruthy;
       expect(recordException).toBeCalledWith(testError);
     });
-  });
 
-  describe('finally hook', () => {
-    it('should not call end because the span is undefined', () => {
-      otelHook.finally(hookContext);
-      expect(otelHook['spanMap'].has(hookContext)).toBeFalsy;
-      expect(end).not.toBeCalled();
-    });
-
-    it('should call end to finish the span', () => {
-      otelHook.before(hookContext);
-      otelHook.finally(hookContext);
-      expect(otelHook['spanMap'].has(hookContext)).toBeTruthy;
-      expect(end).toBeCalled();
+    it('should not call recordException because there is no active span', () => {
+      getActiveSpan.mockReturnValueOnce(undefined);
+      otelHook.error(hookContext, testError);
+      expect(recordException).not.toBeCalled();
     });
   });
 });
