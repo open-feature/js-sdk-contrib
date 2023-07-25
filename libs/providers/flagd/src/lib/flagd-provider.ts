be76d16e755456032a4dbf0496f0e8203cd57e85
Todd Baert <todd.baert@dynatrace.com>
2023-07-25 13:52:48 -0400

    fixup: review feedback
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- libs/providers/flagd/src/lib/flagd-provider.ts
+++ libs/providers/flagd/src/lib/flagd-provider.ts
@@ -1,4 +1,13 @@
-import { EvaluationContext, JsonValue, Logger, OpenFeatureEventEmitter, Provider, ProviderEvents, ProviderStatus, ResolutionDetails } from '@openfeature/js-sdk';
+import {
+  EvaluationContext,
+  JsonValue,
+  Logger,
+  OpenFeatureEventEmitter,
+  Provider,
+  ProviderEvents,
+  ProviderStatus,
+  ResolutionDetails,
+} from '@openfeature/js-sdk';
 import { FlagdProviderOptions, getConfig } from './configuration';
 import { GRPCService } from './service/grpc/service';
 import { Service } from './service/service';
@@ -17,34 +26,41 @@ export class FlagdProvider implements Provider {
   }
 
   private readonly _service: Service;
-  private _status = ProviderStatus.NOT_READY; 
+  private _status = ProviderStatus.NOT_READY;
   private _events = new OpenFeatureEventEmitter();
 
-  constructor(options?: FlagdProviderOptions, service?: Service, private logger?: Logger) {
+  constructor(
+    options?: FlagdProviderOptions,
+    service?: Service,
+    private logger?: Logger,
+  ) {
     this._service = service ? service : new GRPCService(getConfig(options), undefined, logger);
   }
 
   initialize(): Promise<void> {
-    return this._service.connect(this.setReady.bind(this), this.emitChanged.bind(this), this.setError.bind(this)).then(() => {
-      this.logger?.debug(`${this.metadata.name}: ready`);
-      this._status = ProviderStatus.READY;
-    }).catch((err) => {
-      this._status = ProviderStatus.ERROR;
-      this.logger?.error(`${this.metadata.name}: error during initialization: ${err.message}, ${err.stack}`);
-      throw err;
-    });
+    return this._service
+      .connect(this.setReady.bind(this), this.emitChanged.bind(this), this.setError.bind(this))
+      .then(() => {
+        this.logger?.debug(`${this.metadata.name}: ready`);
+        this._status = ProviderStatus.READY;
+      })
+      .catch((err) => {
+        this._status = ProviderStatus.ERROR;
+        this.logger?.error(`${this.metadata.name}: error during initialization: ${err.message}, ${err.stack}`);
+        throw err;
+      });
   }
 
   onClose(): Promise<void> {
-    this.logger?.debug(`${this.metadata.name}: shutting down`)
-    return this._service.disconnect();  
+    this.logger?.debug(`${this.metadata.name}: shutting down`);
+    return this._service.disconnect();
   }
 
   resolveBooleanEvaluation(
     flagKey: string,
     _: boolean,
     transformedContext: EvaluationContext,
-    logger: Logger
+    logger: Logger,
   ): Promise<ResolutionDetails<boolean>> {
     return this._service
       .resolveBoolean(flagKey, transformedContext, logger)
@@ -55,7 +71,7 @@ export class FlagdProvider implements Provider {
     flagKey: string,
     _: string,
     transformedContext: EvaluationContext,
-    logger: Logger
+    logger: Logger,
   ): Promise<ResolutionDetails<string>> {
     return this._service
       .resolveString(flagKey, transformedContext, logger)
@@ -66,7 +82,7 @@ export class FlagdProvider implements Provider {
     flagKey: string,
     _: number,
     transformedContext: EvaluationContext,
-    logger: Logger
+    logger: Logger,
   ): Promise<ResolutionDetails<number>> {
     return this._service
       .resolveNumber(flagKey, transformedContext, logger)
@@ -77,7 +93,7 @@ export class FlagdProvider implements Provider {
     flagKey: string,
     _: T,
     transformedContext: EvaluationContext,
-    logger: Logger
+    logger: Logger,
   ): Promise<ResolutionDetails<T>> {
     return this._service
       .resolveObject<T>(flagKey, transformedContext, logger)
