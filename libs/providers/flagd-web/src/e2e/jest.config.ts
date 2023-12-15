09efeaa21a7bd5aff2318b2e37655676f62ce692
Todd Baert <todd.baert@dynatrace.com>
2023-12-15 15:41:05 -0500

    fixup: jest config
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- libs/providers/flagd-web/src/e2e/jest.config.ts
+++ libs/providers/flagd-web/src/e2e/jest.config.ts
@@ -3,6 +3,9 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsConfig: './tsconfig.lib.json' }],
   },
+  moduleNameMapper: {
+    '^(.*)\\.js$': ['$1.js', '$1.ts', '$1']
+  },
   testEnvironment: 'node',
   preset: 'ts-jest',
   clearMocks: true,
