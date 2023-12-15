77007eaad04f3549c84c55e2b9648d0f1f9dfc2c
Todd Baert <todd.baert@dynatrace.com>
2023-12-15 15:44:00 -0500

    fixup: lint
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- libs/providers/flagd-web/src/e2e/jest.config.ts
+++ libs/providers/flagd-web/src/e2e/jest.config.ts
@@ -4,7 +4,7 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsConfig: './tsconfig.lib.json' }],
   },
   moduleNameMapper: {
-    '^(.*)\\.js$': ['$1.js', '$1.ts', '$1']
+    '^(.*)\\.js$': ['$1.js', '$1.ts', '$1'],
   },
   testEnvironment: 'node',
   preset: 'ts-jest',
