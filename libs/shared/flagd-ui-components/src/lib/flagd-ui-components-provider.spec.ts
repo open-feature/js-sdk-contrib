e058e688484612f3944d65ce53a6c0f899dc3c76
Todd Baert <todd.baert@dynatrace.com>
2024-04-27 10:48:08 -0400

    test-app

--- libs/shared/flagd-ui-components/src/lib/flagd-ui-components-provider.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FlagdUiComponentsProvider } from './flagd-ui-components-provider';
-
-describe('FlagdUiComponentsProvider', () => {
-  it('should be and instance of FlagdUiComponentsProvider', () => {
-    expect(new FlagdUiComponentsProvider()).toBeInstanceOf(FlagdUiComponentsProvider);
-  });
-});
