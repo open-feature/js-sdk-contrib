4a3e573e18da864bd5c3756bdf115e92b7749b21
Chris Price <6090660+crprice@users.noreply.github.com>
2025-01-22 12:35:46 -0800

    Add unit test to goff-api.spec.ts for support endpoint paths in data collector goff-api.ts goff-api.spec.ts
    Signed-off-by: Chris Price <6090660+crprice@users.noreply.github.com>

--- libs/providers/go-feature-flag-web/src/lib/controller/goff-api.spec.ts
+++ libs/providers/go-feature-flag-web/src/lib/controller/goff-api.spec.ts
@@ -103,6 +103,52 @@ describe('Collect Data API', () => {
     );
   });
 
+  it('should call the API to collect data with endpoint path', async () => {
+    fetchMock.post('https://gofeatureflag.org/examplepath/v1/data/collector', 200);
+    const options: GoFeatureFlagWebProviderOptions = {
+      endpoint: 'https://gofeatureflag.org/examplepath',
+      apiTimeout: 1000,
+    };
+    const goff = new GoffApiController(options);
+    await goff.collectData(
+      [
+        {
+          key: 'flagKey',
+          contextKind: 'user',
+          creationDate: 1733138237486,
+          default: false,
+          kind: 'feature',
+          userKey: 'toto',
+          value: true,
+          variation: 'varA',
+        },
+      ],
+      { provider: 'open-feature-js-sdk' },
+    );
+    expect(fetchMock.lastUrl()).toBe('https://gofeatureflag.org/examplepath/v1/data/collector');
+    expect(fetchMock.lastOptions()?.headers).toEqual({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    });
+    expect(fetchMock.lastOptions()?.body).toEqual(
+      JSON.stringify({
+        events: [
+          {
+            key: 'flagKey',
+            contextKind: 'user',
+            creationDate: 1733138237486,
+            default: false,
+            kind: 'feature',
+            userKey: 'toto',
+            value: true,
+            variation: 'varA',
+          },
+        ],
+        meta: { provider: 'open-feature-js-sdk' },
+      }),
+    );
+  });
+
   it('should not call the API to collect data if no event provided', async () => {
     fetchMock.post('https://gofeatureflag.org/v1/data/collector', 200);
     const options: GoFeatureFlagWebProviderOptions = {
