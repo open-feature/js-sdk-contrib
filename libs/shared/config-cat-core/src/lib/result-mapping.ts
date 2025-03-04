9d14173cf08da3030fc58fea8786b24bafd80403
adams85 <31276480+adams85@users.noreply.github.com>
2025-03-04 10:01:14 +0100

    fix(config-cat): Forward default value to underlying client (#1214)
    Signed-off-by: Adam Simon <adam@configcat.com>
    Co-authored-by: Adam Simon <adam@configcat.com>
    Co-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- libs/shared/config-cat-core/src/lib/result-mapping.ts
+++ libs/shared/config-cat-core/src/lib/result-mapping.ts
@@ -1,16 +1,22 @@
 import { IEvaluationDetails } from 'configcat-js-ssr';
-import { ResolutionDetails, ResolutionReason, TypeMismatchError, StandardResolutionReasons } from '@openfeature/core';
+import {
+  ResolutionDetails,
+  ResolutionReason,
+  TypeMismatchError,
+  StandardResolutionReasons,
+  JsonValue,
+  OpenFeatureError,
+  ParseError,
+  TargetingKeyMissingError,
+  GeneralError,
+  FlagNotFoundError,
+} from '@openfeature/core';
 
-export function toResolutionDetails<T extends PrimitiveTypeName>(
-  type: T,
-  value: unknown,
+export function toResolutionDetails<T>(
+  value: T,
   data: Omit<IEvaluationDetails, 'value'>,
   reason?: ResolutionReason,
-): ResolutionDetails<PrimitiveType<T>> {
-  if (!isType(type, value)) {
-    throw new TypeMismatchError(`Requested ${type} flag but the actual value is ${typeof value}`);
-  }
-
+): ResolutionDetails<T> {
   const matchedTargeting = data.matchedTargetingRule;
   const matchedPercentage = data.matchedPercentageOption;
 
@@ -25,7 +31,30 @@ export function toResolutionDetails<T extends PrimitiveTypeName>(
   };
 }
 
-export type PrimitiveTypeName = 'string' | 'boolean' | 'number' | 'object' | 'undefined';
+export function parseError(errorMessage: string | undefined): OpenFeatureError {
+  // Detecting the error type by checking the error message is awkward and fragile,
+  // but ConfigCat SDK doesn't allow a better way at the moment.
+  // However, there are plans to improve this situation, so let's revise this
+  // as soon as ConfigCat SDK implements returning error codes.
+
+  if (errorMessage) {
+    if (errorMessage.includes('Config JSON is not present')) {
+      return new ParseError();
+    }
+    if (errorMessage.includes('the key was not found in config JSON')) {
+      return new FlagNotFoundError();
+    }
+    if (
+      errorMessage.includes('The type of a setting must match the type of the specified default value') ||
+      /Setting value (?:is null|is undefined|'.*' is of an unsupported type)/.test(errorMessage)
+    ) {
+      return new TypeMismatchError();
+    }
+  }
+  return new GeneralError();
+}
+
+export type PrimitiveTypeName = 'string' | 'boolean' | 'number' | 'object';
 export type PrimitiveType<T> = T extends 'string'
   ? string
   : T extends 'boolean'
@@ -33,11 +62,24 @@ export type PrimitiveType<T> = T extends 'string'
     : T extends 'number'
       ? number
       : T extends 'object'
-        ? object
-        : T extends 'undefined'
-          ? undefined
-          : unknown;
+        ? JsonValue
+        : never;
 
 export function isType<T extends PrimitiveTypeName>(type: T, value: unknown): value is PrimitiveType<T> {
-  return typeof value !== 'undefined' && typeof value === type;
+  switch (type) {
+    case 'string':
+    case 'boolean':
+    case 'number':
+      return typeof value === type;
+    case 'object':
+      return (
+        value === null ||
+        typeof value === 'string' ||
+        typeof value === 'boolean' ||
+        typeof value === 'number' ||
+        Array.isArray(value) ||
+        typeof value === 'object'
+      );
+  }
+  return false;
 }
