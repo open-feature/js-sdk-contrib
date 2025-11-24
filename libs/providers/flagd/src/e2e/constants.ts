b5173233bc09a2778f2290c0932377ff14e85f59
marcozabel <marco.zabel@dynatrace.com>
2025-11-24 18:17:43 +0100

    Remvoe unused constants
    Signed-off-by: marcozabel <marco.zabel@dynatrace.com>

--- libs/providers/flagd/src/e2e/constants.ts
+++ libs/providers/flagd/src/e2e/constants.ts
@@ -1,20 +1,6 @@
 import { getGherkinTestPath } from '@openfeature/flagd-core';
 
-export const FLAGD_NAME = 'flagd';
 export const UNSTABLE_CLIENT_NAME = 'unstable';
 export const UNAVAILABLE_CLIENT_NAME = 'unavailable';
 
 export const GHERKIN_FLAGD = getGherkinTestPath('*.feature');
-export const CONNECTION_FEATURE = getGherkinTestPath('connection.feature');
-export const CONTEXT_ENRICHMENT_FEATURE = getGherkinTestPath('contextEnrichment.feature');
-export const EVALUATION_FEATURE = getGherkinTestPath('evaluation.feature');
-export const EVENTS_FEATURE = getGherkinTestPath('events.feature');
-export const METADATA_FEATURE = getGherkinTestPath('metadata.feature');
-export const RPC_CACHING_FEATURE = getGherkinTestPath('rpc-caching.feature');
-export const SELECTOR_FEATURE = getGherkinTestPath('selector.feature');
-export const SYNC_PAYLOAD_FEATURE = getGherkinTestPath('sync-payload.feature');
-export const TARGETING_FEATURE = getGherkinTestPath('targeting.feature');
-export const GHERKIN_EVALUATION_FEATURE = getGherkinTestPath(
-  'evaluation.feature',
-  'spec/specification/assets/gherkin/',
-);
