ba082e8c0941cae9823170330c6950282aecfd44
Norris <jonathan.norris@dynatrace.com>
2026-02-24 17:47:52 -0500

    chore(flagd-core): move test infra to PR 2 (restricted env tests belong with workers option)
    Signed-off-by: Norris <jonathan.norris@dynatrace.com>

--- libs/shared/flagd-core/jest.config.ts
+++ libs/shared/flagd-core/jest.config.ts
@@ -4,11 +4,8 @@ export default {
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  // Do not attempt to transform plain .js files (generated validators, jest environment).
-  // These are already valid CommonJS and don't need ts-jest processing.
-  transformIgnorePatterns: ['\\.js$'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/shared/flagd-core',
 };
