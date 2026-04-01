a686700d993576c30df8d0df8a4af76f86ad617a
Todd Baert <todd.baert@dynatrace.com>
2026-04-01 15:35:22 -0400

    fixup: update testbed and exclusions
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- libs/providers/flagd/src/e2e/tests/rpc.spec.ts
+++ libs/providers/flagd/src/e2e/tests/rpc.spec.ts
@@ -25,8 +25,7 @@ describe('rpc', () => {
       tagFilter:
         // remove filters as we add support for features
         // see: https://github.com/open-feature/js-sdk-contrib/issues/1096 and child issues
-        // TODO remove last two tags from being excluded once flagd supports the fractional enhancements
-        '@rpc and not @targetURI and not @caching and not @unixsocket and not @deprecated and not @fractional-v1 and not @fractional-v2 and not @fractional-nested',
+        '@rpc and not @targetURI and not @caching and not @unixsocket and not @deprecated and not @fractional-v1 and not @operator-errors',
       scenarioNameTemplate: (vars) => {
         return `${vars.scenarioTitle} (${vars.scenarioTags.join(',')} ${vars.featureTags.join(',')})`;
       },
