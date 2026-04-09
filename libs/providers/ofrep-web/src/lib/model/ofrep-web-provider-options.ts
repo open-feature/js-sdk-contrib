ec258441cb9694820b5cf0c81a7b952ad79d01c8
marcozabel <marco.zabel@dynatrace.com>
2026-04-09 11:45:53 +0200

    refactor: disable polling by default in OFREP web provider
    - Change DEFAULT_POLL_INTERVAL from 30000ms to 0 (disabled by default)
    - Update OFREPWebProviderOptions documentation to reflect polling is now opt-in
    - Polling must be explicitly enabled by setting pollInterval to a positive number
    - This is a breaking change, appropriate for sub-v1 OFREP providers
    
    Signed-off-by: marcozabel <marco.zabel@dynatrace.com>

--- libs/providers/ofrep-web/src/lib/model/ofrep-web-provider-options.ts
+++ libs/providers/ofrep-web/src/lib/model/ofrep-web-provider-options.ts
@@ -5,8 +5,9 @@ export type OFREPWebProviderOptions = OFREPProviderBaseOptions & {
    * pollInterval is the time in milliseconds to wait between we call the OFREP
    * API to get the latest evaluation of your flags.
    *
-   * If a negative number is provided, the provider will not poll the OFREP API.
-   * Default: 30000
+   * If a negative number or 0 is provided, the provider will not poll the OFREP API.
+   * This is the default behavior. Polling is available as an opt-in configuration.
+   * Default: 0 (disabled)
    */
   pollInterval?: number; // in milliseconds
 };
