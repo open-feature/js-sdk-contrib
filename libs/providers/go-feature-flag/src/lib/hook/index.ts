dbc70d0767ba0873a1701ffc62ae5e23aa2030ea
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2026-08-07 21:35:53 +0200

    chore(go-feature-flag): export the DataCollectorHook options type
    DataCollectorHookOptions is part of the exported class's constructor
    signature, so the barrel should re-export it alongside the class.
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- libs/providers/go-feature-flag/src/lib/hook/index.ts
+++ libs/providers/go-feature-flag/src/lib/hook/index.ts
@@ -1,2 +1,3 @@
 export { DataCollectorHook } from './data-collector-hook';
+export type { DataCollectorHookOptions } from './data-collector-hook';
 export { EnrichEvaluationContextHook } from './enrich-evaluation-context-hook';
